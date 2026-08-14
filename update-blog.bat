b527c54887f2063420561b3629ca2f6fb35c126d
FxzClh88 <1468703061@qq.com>
2026-08-14 14:11:20 +0800

    update: switch update-blog.bat to git push workflow

--- update-blog.bat
+++ update-blog.bat
@@ -3,32 +3,25 @@ setlocal
 chcp 65001 >nul
 title Update Blog
 
-rem Clear NODE_OPTIONS to avoid the safe-delete shim breaking hexo deploy
-set NODE_OPTIONS=
-set NODE=C:\Users\Administrator\.workbuddy\binaries\node\versions\22.22.2\node.exe
-
 cd /d C:\Users\Administrator\myblog
 
 echo ============================================
-echo    Update Blog & Deploy to GitHub Pages
+echo    Update Blog via GitHub Actions
 echo ============================================
 echo.
 
-echo [1/3] Cleaning cache...
-%NODE% node_modules\hexo\bin\hexo clean
-echo.
+echo [1/2] Staging changes...
+git add -A
 
-echo [2/3] Generating static files...
-%NODE% node_modules\hexo\bin\hexo generate
-echo.
+echo [2/2] Commit & Push to source branch...
+git commit -m "Update blog" >nul 2>&1
+git push origin source
 
-echo [3/3] Deploying to GitHub Pages...
-%NODE% node_modules\hexo\bin\hexo deploy
 echo.
-
 echo ============================================
-echo    Done! Visit https://FxzClh88.github.io
-echo    (Wait 1-2 min for GitHub to build)
+echo    Done! GitHub Actions will build & deploy.
+echo    Wait 1-2 min, then visit:
+echo    https://FxzClh88.github.io
 echo ============================================
 echo.
 pause
